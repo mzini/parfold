b21f0af5055dbb6bd468048e95ab5e82214f0eef
martin.avanzini <martin.avanzini@uibk.ac.at>
2009-05-04 10:43:32 +0200

    initial import
    Ignore-this: f8b557de80413816081a25a709e2ae78
    
    darcs-hash:20090504084332-3c1c7-42b7fd2929f1403418727f659082139a671e1cc6.gz

---
+++ Control/Concurrent/PFold.hs
@@ -0,0 +1,44 @@
+module Control.Concurrent.PFold 
+ ( Return (..)
+ , pfoldA
+ , pfold
+ , fastest
+ , fastestSatisfying
+ )
+where
+import Control.Concurrent (forkIO, killThread)
+import Control.Parallel (pseq)
+import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
+
+data Return a = Finished a
+              | Continue a
+
+pfoldA :: (a -> b -> Return a) -> a -> [IO b] -> IO a
+pfoldA f e ios = do
+  mv <- newEmptyMVar 
+  tids <- mapM (spwn mv) ios
+  r <- collect mv tids e
+  killAll tids
+  return r
+    where spwn mv io = forkIO $ do b <- io
+                                   pseq b (return ())
+                                   putMVar mv b
+
+          collect _  []       a = return a
+          collect mv (_:tids) a = 
+              do b <- takeMVar mv
+                 case f a b of 
+                   Finished r -> return r
+                   Continue r -> collect mv tids r
+
+          killAll = mapM killThread
+
+pfold :: (a -> b -> a) -> a -> [IO b] -> IO a
+pfold f = pfoldA $ \ a b -> Continue (f a b)
+
+fastest :: a -> [IO a] -> IO a
+fastest = fastestSatisfying $ const True
+
+fastestSatisfying :: (a -> Bool) -> a -> [IO a] -> IO a
+fastestSatisfying f = pfoldA (\ a b -> if f b then Finished b else Continue a)
+
