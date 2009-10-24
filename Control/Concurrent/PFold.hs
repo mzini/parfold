5f093b4db864e6718ffb2799dcfdc99d71ef9864
martin.avanzini <martin.avanzini@uibk.ac.at>
2009-10-24 16:34:51 +0200

    anotated licence
    Ignore-this: 3d197596db51be696282ee0ca4507bde
    
    darcs-hash:20091024143451-3c1c7-18c71047160dda55f486c702caa2186e86eed7a7.gz

--- Control/Concurrent/PFold.hs
+++ Control/Concurrent/PFold.hs
@@ -1,3 +1,20 @@
+{-
+This file is part of the Haskell Parfold Library.
+
+The Haskell Parfold Library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The Haskell Parfold Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Haskell Parfold Library.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Control.Concurrent.PFold 
  ( Return (..)
  , pfoldA
