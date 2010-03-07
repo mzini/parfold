4ba0f4b866745b4ac01318f669c6addd730155cb
martin.avanzini <martin.avanzini@uibk.ac.at>
2010-03-07 10:59:26 +0100

    split into pfold and utils
    Ignore-this: 47311e4e49648f7c775264322a6b997b
    
    darcs-hash:20100307095926-3c1c7-dfcc193f691e842ae9665059a7608a72eeb05c5f.gz

--- Control/Concurrent/PFold.hs
+++ Control/Concurrent/PFold.hs
@@ -1,22 +1,37 @@
+{-
+This file is part of the Haskell Parfold Library.
+
+The Haskell Parfold Library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The Haskell Parfold Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Haskell Parfold Library.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Control.Concurrent.PFold 
- -- ( Return (..)
- -- , pfoldA
- -- , pfold
- -- , fastest
- -- , fastestSatisfying
- -- , choice
- -- )
+ ( Return (..)
+ , pfoldA
+ , pfold
+ , fastest
+ , fastestSatisfying
+ , choice
+ , par
+ , alt
+ , (<+>)
+ , (<|>))
 where
-import Control.Concurrent (forkIO, killThread, myThreadId, threadDelay)
+import Control.Concurrent (forkIO, killThread)
 import Control.Parallel (pseq)
-import System.Process
-import System.Exit	( ExitCode(..) )
-import System.IO.Error
 import qualified Control.Exception as C
 import Control.Monad
 import System.IO
-import Data.Typeable
-import System.Timeout
 import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 
 data Return a = Stop a
@@ -50,10 +65,10 @@ fastestSatisfying :: (a -> Bool) -> a -> [IO a] -> IO a
 fastestSatisfying f = pfoldA (\ a b -> if f b then Stop b else Continue a)
 
 
-runpar :: IO a -> IO b -> IO (a, b)
-runpar ma mb = do Just (Right r) <- pfoldA plus Nothing [ Left `liftM` ma
-                                                       , Right `liftM` mb]
-                  return r
+par :: IO a -> IO b -> IO (a, b)
+par ma mb = do Just (Right r) <- pfoldA plus Nothing [ Left `liftM` ma
+                                                    , Right `liftM` mb]
+               return r
     where plus Nothing                 b         = Continue $ Just $ Left b
           plus (Just (Left (Left a)))  (Right b) = Stop $ Just $ Right (a, b)
           plus (Just (Left (Right b))) (Left a)  = Stop $ Just $ Right (a, b)
@@ -67,66 +82,5 @@ alt m ms = do Just r <- pfoldA plus Nothing (m:ms)
 choice :: IO a -> IO a -> IO a
 choice a b = alt a [b]
 
-(<+>) = runpar
+(<+>) = par
 (<|>) = choice
-
-
-spawn :: String -> [String] -> String -> IO (Maybe String)
-spawn cmd args input = do (Just inh, Just outh, _, pid) <- createProcess (proc cmd args){ std_in  = CreatePipe,
-                                                                                         std_out = CreatePipe,
-                                                                                         std_err = Inherit }
-                          catchErr inh outh pid (spwn inh outh pid)
-    where spwn inh outh pid = do output  <- hGetContents outh
-                                 outMVar <- newEmptyMVar
-                                 forkIO $ C.evaluate (length output) >> putMVar outMVar ()
-
-                                 when (not (null input)) $ do hPutStr inh input; hFlush inh
-                                 takeMVar outMVar
-                                 finalize inh outh
-                                 ex <- waitForProcess pid
-                                 case ex of
-                                   ExitSuccess   -> return $ Just output
-                                   ExitFailure r -> return Nothing
-
-          finalize inh outh = hClose inh >> hClose outh
-          
-          catchErr inh outh pid ma = C.catchJust threadKilled ma handler
-              where handler () = do terminateProcess pid
-                                    waitForProcess pid
-                                    finalize inh outh
-                                    putStrLn "handler called"
-                                    return $ Nothing
-
--- ls :: String -> IO (Maybe [String])
--- ls str = (liftM lines) `liftM` spawn "ls" [str] ""
-
--- find :: String -> IO (Maybe [String])
--- find str = (liftM lines) `liftM` spawn "find" [str] ""
-
-
-data Timeout = Timeout deriving (Show, Typeable)
--- instance C.Exception Timeout 
-
-timedKill :: Int -> IO a -> IO (Maybe a)
-timedKill n m = do pid <- myThreadId
-                   C.handleJust 
-                    threadKilled 
-                    (const $ return Nothing)
-                    (C.bracket 
-                          (forkIO $ threadDelay n >> killThread pid)
-                          killThread
-                          (const $ C.unblock m >>= return . Just))
-
-threadKilled :: C.Exception -> Maybe ()
-threadKilled e = case cast e of 
-                   Just C.ThreadKilled -> Just ()
-                   _ -> Nothing
-
-
-
-fib 0 = return 0
-fib 1 = return 1
-fib 2 = return 3
-fib n = do a <- fib (n - 1)
-           b <- fib (n - 2)
-           return $ a + b
